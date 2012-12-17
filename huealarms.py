927e309575cb125366cdf39fd23980d183126a69
Malcolm Crum <crummynz@gmail.com>
2012-12-16 19:21:19 -0800

    Web interface now prints lights

--- huealarms.py
+++ huealarms.py
@@ -5,13 +5,12 @@ urls = (
 	'/', 'index'
 	)
 bridge = Bridge('192.168.1.42', 'e9a353626dcf16710e53f9a2ae2407b')
-bridge.connect()
 bridge.get_api()
 
 class index:
 	def GET(self):
-		bridge.get_light(1, 'on')
-		return "Hello, world!"
+		print bridge.get_light_objects('name')
+		return bridge.get_light_objects('name')
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
